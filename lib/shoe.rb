731626bf04f782c6f23be08661f998ce13dfaecf
Adam Priest <sepone229@gmail.com>
2020-03-18 10:13:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,7 +4,7 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
-    BRANDS << brand if BRANDS.select{ |b| b
+    BRANDS << brand if BRANDS.select{ |b| b != brand}
   end
 
   def cobble
